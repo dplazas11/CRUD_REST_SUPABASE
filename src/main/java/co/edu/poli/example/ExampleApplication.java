2ca6d2209a72f3c671d58b09e902668aafb0ce55
dplazas <dplazas@poligran.edu.co>
2025-10-17 22:16:48 -0500

    actualizacion info base de datos

--- src/main/java/co/edu/poli/example/ExampleApplication.java
+++ src/main/java/co/edu/poli/example/ExampleApplication.java
@@ -9,6 +9,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ExampleApplication {
 
 	public static void main(String[] args) {	
+		
 		Dotenv dotenv = Dotenv.configure().load();
         System.setProperty("DB_USER_SUPABASE", dotenv.get("DB_USER_SUPABASE"));
         System.setProperty("DB_PWD_SUPABASE", dotenv.get("DB_PWD_SUPABASE"));	
